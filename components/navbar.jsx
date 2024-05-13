5eaa12ef05658d57583382c1fc7c5b44eace071c
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-05-13 14:30:43 +0200

    portfolio fini

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <h1>Nikolaï ZLODEYEV</h1>
       <ul className="nav-links">
+        <div className='name'>Nikolaï ZLODEYEV</div>
         <li><a href="#">Home</a></li>
         <li><a href="#">About</a></li>
         <li><a href="#">Contact</a></li>
--- pages/index.js
+++ pages/index.js
@@ -16,17 +16,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <div>
+      <img src="\photo-etudiant.jpg" className="photo-etudiant" />
+    </div>
       <div className="portfolio-text">
-      <p>Hi !</p><br />
-      <p>I'm <span style={{color: "blue" }}> Nikolaï</span></p><br />
+      <p>Hi !</p>
+      <p>I'm <span style={{color: "blue" }}> Nikolaï</span></p>
       <p>Web Developper</p>
       </div>
 
-      <button>
-        <a>Me contacter</a>
+      <button className="bouton-contact">
+        <a href="https://imgs.search.brave.com/XaUuZptfk1FiJY4zzhL33g2qk2C1gvuqvJ6zAWGne5s/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdDIu/ZGVwb3NpdHBob3Rv/cy5jb20vMTAwMDU4/OS83OTgyL2kvNDUw/L2RlcG9zaXRwaG90/b3NfNzk4MjUwMjAt/c3RvY2stcGhvdG8t/YmVlZi1zdGVha3Mt/b24td29vZGVuLWJv/YXJkLmpwZw">Me contacter</a>
       </button>
 
-      <img src="\photo-etudiant.jpg" className="photo-etudiant" />
     </>
   );
 }
