008a5486ce4d6a7008cdaa82bb15173f0b957638
Medve-d <nikolai.zlodeyev@ecole-isitech.fr>
2024-05-13 12:38:46 +0200

    portfolio

---
+++ components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+        <h1>Nikolaï ZLODEYEV</h1>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
